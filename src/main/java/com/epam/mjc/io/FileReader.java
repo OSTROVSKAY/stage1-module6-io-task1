996cb601a8015593aa713a42fc2a0a6241fd29a8
OSTROVSKAY <OSTROVSKAY01989@gmail.com>
2023-12-10 23:53:38 +0300

    Finish : Task IO

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -1,62 +1,85 @@
 package com.epam.mjc.io;
 
 import java.io.File;
-
 import java.io.FileInputStream;
-
+import java.io.FileNotFoundException;
 import java.io.IOException;
+
 
 public class FileReader {
 
-    public Profile getDataFromFile(File file) {
+    public Profile getDataFromFile(File file)  {
+
+        FileInputStream stream;
+        stream = null;
 
         StringBuilder profileData = new StringBuilder();
 
         try {
-
-            FileInputStream stream = new FileInputStream(file);
+             stream = new FileInputStream(file);
 
             int i;
 
             while ((i = stream.read()) != -1) {
-
                 profileData.append((char) i);
-
             }
-
-            stream.close();
 
         } catch (IOException e) {
 
-            System.out.println("Problem with read file!");
+
+            System.err.println("Problem with read file!");
+        } finally {
+
+            if(stream != null) {
+
+                try {
+                    stream.close();
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
 
         }
 
         String string = profileData.toString();
-
         String[] splitStrings = string.split("\r\n");
 
+
+
         String[] profileFields = new String[splitStrings.length];
+
+
 
         for (int i = 0; i < splitStrings.length; i++) {
 
             String[] tempArray = splitStrings[i].split(" ");
 
-            profileFields[i] = tempArray[1];
+            if(tempArray.length > 1) {
+
+                profileFields[i] = tempArray[1];
+
+            } else if(tempArray.length == 1) {
+
+                profileFields[i] = tempArray[0];
+
+            }
+
 
         }
 
-        Profile profile = new Profile();
+        Profile profile = new Profile(
+                profileFields[0],
+                Integer.parseInt(profileFields[1]),
+                profileFields[2],
+                Long.parseLong(profileFields[3])
+        );
 
-        profile.setName(profileFields[0]);
 
-        profile.setAge(Integer.parseInt(profileFields[1]));
 
-        profile.setEmail(profileFields[2]);
 
-        profile.setPhone(Long.parseLong(profileFields[3]));
-
+       
         return profile;
-
+        
     }
+
 }
