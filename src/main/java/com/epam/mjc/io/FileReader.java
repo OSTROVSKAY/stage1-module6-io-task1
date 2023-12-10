95cdca9a186e780de76a83a99c4c12a9f79d0842
OSTROVSKAY <OSTROVSKAY01989@gmail.com>
2023-12-10 21:24:39 +0300

    Finish : Task IO

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -2,10 +2,63 @@ package com.epam.mjc.io;
 
 import java.io.File;
 
+import java.io.FileInputStream;
+
+import java.io.IOException;
 
 public class FileReader {
 
     public Profile getDataFromFile(File file) {
-        return new Profile();
+
+        StringBuilder profileData = new StringBuilder();
+
+        try {
+
+            FileInputStream stream = new FileInputStream(file);
+
+            int i;
+
+            while ((i = stream.read()) != -1) {
+
+                profileData.append((char) i);
+
+            }
+
+            stream.close();
+
+        } catch (IOException e) {
+
+            System.out.println("Problem with read file!");
+
+        }
+
+        String string = profileData.toString();
+
+        String[] splitStrings = string.split("\r\n");
+
+        String[] profileFields = new String[splitStrings.length];
+
+        for (int i = 0; i < splitStrings.length; i++) {
+
+            String[] tempArray = splitStrings[i].split(" ");
+
+            profileFields[i] = tempArray[1];
+
+        }
+
+        Profile profile = new Profile(
+
+                profileFields[0],
+
+                Integer.parseInt(profileFields[1]),
+
+                profileFields[2],
+
+                Long.parseLong(profileFields[3])
+
+        );
+
+        return profile;
+
     }
 }
